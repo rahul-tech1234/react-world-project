dfac8d5ed927ad86a5cc627e262d17ea49add989
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-05-12 22:06:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Countries from "./component/Countries/Countries";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+        <Countries></Countries>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Countries/Countries.jsx
@@ -0,0 +1,9 @@
+const Countries = () => {
+    return (
+        <div>
+            <h1>in the country</h1>            
+        </div>
+    );
+};
+
+export default Countries;
